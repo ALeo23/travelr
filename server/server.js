f8d8a655137b3cf7e9d182fd22668a25e89db124
Alex Leo <aleo1337@gmail.com>
2016-10-17 17:48:26 -0400

    Add response for favicon

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
   res.send('Hello world!');
 });
 
+app.get('/favicon.ico', function(req, res) {
+  res.send('No image available')
+});
+
 app.listen(3000, function() {
   console.log('Hi Alex, app in now listening on port 3000');
 });
