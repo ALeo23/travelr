a05644d017d9b0c6b6850cb22375f1d30b9af2f5
Alex Leo <aleo1337@gmail.com>
2016-10-17 18:10:44 -0400

    Configure server to use dynamic port

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/../client'));
 
 app.get('/', function(req, res) {
@@ -12,6 +14,6 @@ app.get('/favicon.ico', function(req, res) {
   res.send('No image available');
 });
 
-app.listen(3000, function() {
-  console.log('Hi Alex, app in now listening on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Hi Alex, app in now listening on port ' + app.get('port'));
 });
