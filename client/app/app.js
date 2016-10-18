46de169728a1cdc693a54a4c0c9b7b4c09611433
Alex Leo <aleo1337@gmail.com>
2016-10-17 22:09:58 -0400

    Create factory and controller to pull live data from Yahoo weather

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,37 @@
-angular.module('travelr', [
-  'travelr.weather',
-  'travelr.services'
-]);
+var travelr = angular.module('travelr', []);
+
+travelr.factory('WeatherStats', ['$http', '$q', function($http, $q) {
+
+  function getWeather(zipCode) {
+    var deferred = $q.defer();
+    $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+      .success(function(data) {
+        deferred.resolve(data.query.results.channel);
+      })
+      .error(function(err) {
+        console.log('Error retrieving data.');
+        deferred.reject(err);
+      });
+      return deferred.promise;
+
+  }
+  return {
+    getWeather: getWeather
+  };
+}]);
+
+travelr.controller('WeatherCtrl', ['$scope', 'WeatherStats', function($scope, WeatherStats) {
+  function fetchWeather(zipCode) {
+    WeatherStats.getWeather(zipCode).then(function(data) {
+      $scope.place = data;
+    });
+  }
+
+  fetchWeather('94101');
+
+  $scope.findWeather = function(zipCode) {
+    $scope.place = '';
+    fetchWeather(zipCode);
+  };
+
+}]);
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,18 +1,1 @@
-angular.module('travelr.services', [])
-
-.factory('WeatherStats', function($http) {
-
-  var getWeather = function() {
-    return $http({
-      method: 'GET',
-      url: 'http://api.wunderground.com/api/e2dc2d6bfc0da812/forecast/q/CA/San_Francisco.json'
-    })
-    .then(function(resp) {
-      return resp.forecast;
-    });
-
-  };
-  return {
-    getWeather: getWeather
-  };
-});
+angular.module('travelr.services', []);
